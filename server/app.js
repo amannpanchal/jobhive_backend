381d5bce6d45411326760396bb9d0f627fde7772
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-04-16 23:24:14 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,6 +9,13 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 const { mongooseConnect } = require("../database/dbConnect");
 mongooseConnect();
