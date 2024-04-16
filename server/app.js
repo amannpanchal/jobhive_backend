7ff029b5e36d70033bbbf45f43d533845b062b95
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-04-16 23:29:46 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,35 +1,35 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const cors = require("cors");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { mongooseConnect } = require('../database/dbConnect');
+
 const app = express();
 require('dotenv').config();
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 
-const { mongooseConnect } = require("../database/dbConnect");
 mongooseConnect();
 
+app.use(cors({
+  origin: '*', 
+  credentials: true,
+}));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended : true
-}))
-
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const userRouter = require('../routes/User')
-const applicationRouter = require('../routes/Application')
-const jobRouter = require('../routes/Job')
-app.use('/api/v1/user', userRouter)
-app.use('/api/v1/application',applicationRouter)
+
+// Routes
+const userRouter = require('../routes/User');
+const applicationRouter = require('../routes/Application');
+const jobRouter = require('../routes/Job');
+
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/application', applicationRouter);
 app.use('/api/v1/job', jobRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 module.exports = app;
