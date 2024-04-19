b44efded75f91916404ba378559e15b7ec5cb1cd
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-04-19 08:53:58 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,20 +8,24 @@ require('dotenv').config();
 
 
 mongooseConnect();
-
-app.use(cors({
-  origin: process.env.FRONTEND_URL, 
-  credentials: true,
-}));
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONT_END,
+    credentials: true,
+  })
+);
+
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header("Access-Control-Allow-Origin",   process.env.FRONT_END);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
