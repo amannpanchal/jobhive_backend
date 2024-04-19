ad700cbd6efa9771911ae258bdca2c7bf1fd0623
Aman <128085412+amannpanchal@users.noreply.github.com>
2024-04-19 08:58:27 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,14 +14,14 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONT_END,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
 
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin",   process.env.FRONT_END);
+  res.header("Access-Control-Allow-Origin",   process.env.FRONTEND_URL);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
